92b6bf5bcfa8b78bcd137a8d6217aa8ff0bceb0c
Lê Duy Phong <0977dp@gmail.com>
2026-06-14 09:05:58 +0700

    git add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+use Illuminate\Http\Request;
+use App\Http\Controllers\DemoController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\BrandController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\ProductController;
+Route::get('/test', function () {
+    return "Test";
 });
+// demo
+
+Route::get('/demo', [DemoController::class, 'index']);
+Route::get('/demo2', [DemoController::class, 'index2']);
+Route::get('/demo3', [DemoController::class, 'index3']);
+Route::get('/demo4/{id}', [DemoController::class, 'index4']);
+Route::get('/demo5/{id?}', [DemoController::class, 'index5']);
+Route::get('/demo6/{param1}/{param2}', [DemoController::class, 'index6']);
+
+//CRUD
+Route::prefix('admin')->group(function () {
+    Route::resource('category', CategoryController::class);
+    Route::resource('post', PostController::class);
+    Route::resource('brand', BrandController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('product', ProductController::class);
+});
+
